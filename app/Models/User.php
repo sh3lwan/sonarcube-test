cfe4d8d38bbce30f2bd2b158c0ebd76e5e94e953
Shaban Olwan <sh.3ban@live.com>
2023-05-18 20:10:48 +0300

    Remove Password

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,6 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    protected string $password = "00000";
     /**
      * The attributes that are mass assignable.
      *
